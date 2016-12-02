b9d29dfcffc0058667edfdd2381aca145026d2a1
Lorelei Lopez <email": null>
2016-12-02 16:31:30 -0500

    best i can do

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'nokogiri'
 require 'net/http'
 
-#My API
+# #My API
 jurassic_park_response = open("http://www.omdbapi.com/?t=Jurassic+Park&y=&plot=short&r=json").read
 jurassic_parsed_response = JSON.parse(jurassic_park_response)
 jurassic_movie = Movie.create(title: jurassic_parsed_response["Title"], year: jurassic_parsed_response["Year"], imdbID: jurassic_parsed_response["imdbID"])
@@ -19,3 +19,26 @@ deck = Deck.create(name: 'Movie Deck 1')
 jurassic_park_quotes.each do |quote|
   Quote.create(text: quote.strip, movie_id: 1, deck_id: 1)
 end
+
+# # ----------------------------------------
+# # Anchorman: The Legend of Ron Burgundy
+# #My API
+# anchorman_response = open("http://www.omdbapi.com/?i=tt0357413&plot=short&r=json").read
+# anchorman_parsed_response = JSON.parse(anchorman_response)
+# anchorman_movie = Movie.create(title: anchorman_parsed_response["Title"], year: anchorman_parsed_response["Year"], imdbID: anchorman_parsed_response["imdbID"])
+#
+# #Nokogiri specific page
+# anchorman_imdb_quote_page =  Net::HTTP.get(URI('http://www.imdb.com/title/tt0357413/quotes'))
+# anchorman_quotes = Nokogiri.parse(anchorman_imdb_quote_page)
+# anchorman_quotes = anchorman_quotes.css('.sodatext')
+# anchorman_quotes = anchorman_quotes.map(&:inner_text)
+#
+# #added a deck id and initial user
+# user =  User.create(username: 'lori', password: 'yuca88', email: 'lori@gmail.com')
+# deck = Deck.create(name: 'Movie Deck 1')
+#
+# anchorman_quotes.each do |quote|
+#   Quote.create(text: quote.strip, movie_id: 1, deck_id: 1)
+# end
+
+# Anchorman: The Legend of Ron Burgundy
